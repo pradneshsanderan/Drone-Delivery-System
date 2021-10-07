0787022c4e7287a05a2d1867ff059a49522a609e
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2021-10-07 17:52:47 +0100

    created longlat class + added method frame

---
+++ src/main/java/uk/ac/ed/inf/LongLat.java
@@ -0,0 +1,29 @@
+package uk.ac.ed.inf;
+
+public class LongLat {
+    public double longitude;
+    public double latitude;
+
+    LongLat(double longitude,double latitude){
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    boolean isConfined(){
+
+    }
+
+    double distanceTo(LongLat l1,LongLat l2){
+
+    }
+
+    boolean closeTo(LongLat l1){
+
+    }
+
+    LongLat nextPosition(int angle){
+        
+    }
+
+
+}

9206e1085817907b070d6baea4e781eaf784c903
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2021-10-08 14:53:29 +0100

    added menus and test

--- src/main/java/uk/ac/ed/inf/Menus.java
+++ src/main/java/uk/ac/ed/inf/Menus.java
@@ -1,4 +1,14 @@
 package uk.ac.ed.inf;
 
 public class Menus {
+    public String name;
+    public String port;
+
+    Menus(String name, String port){
+        this.name = name;
+        this.port = port;
+    }
+    public static int  getDeliveryCost(){
+
+    }
 }

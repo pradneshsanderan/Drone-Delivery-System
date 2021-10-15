238b85a41b7bf1eab3009596a1310e781f110a80
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2021-10-15 14:54:03 +0100

    added http client

--- src/main/java/uk/ac/ed/inf/Menus.java
+++ src/main/java/uk/ac/ed/inf/Menus.java
@@ -1,6 +1,20 @@
 package uk.ac.ed.inf;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Menus {
+
+    private static final String urlString = "http://localhost:9898/menus/menus.json";
+    private static final HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(urlString))
+                        .build();
+    HttpResponse<String> response =
+            client.send(request, HttpResponse.BodyHandlers.ofString());
     public String name;
     public String port;
 
@@ -9,7 +23,7 @@ public class Menus {
      * @param name
      * @param port
      */
-    Menus(String name, String port){
+    Menus(String name, String port) throws IOException, InterruptedException {
         this.name = name;
         this.port = port;
     }
@@ -20,7 +34,7 @@ public class Menus {
      * @return
      */
     public static int  getDeliveryCost(String ... items){
-        
+        int standardDeliveryCharge = 50;
         return 1;
     }
 }
